1651e316ef68e93a8b9232cd8612198500638082
Adams408 <jnadams408@gmail.com>
2020-04-22 17:40:36 -0400

    update

--- code/app/app.py
+++ code/app/app.py
@@ -1,8 +1,14 @@
+import pandas as pd
+import os
+import pickle
 import threading
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
 @app.route('/')
@@ -12,7 +18,16 @@ def home():
 
 @app.route('/industry')
 def industry():
-    return render_template('industry.html')
+    if request.method == 'GET':
+        return render_template('industry.html')
+    if request.method == 'POST':
+        if os.path.exists(os.path.join(DATA_DIR, "symbols.pickle")):
+            with open(os.path.join(DATA_DIR, "symbols.pickle"), "rb") as f:
+                symbols = pickle.load(f)
+        symbol = symbols[0]
+        return jsonify(pd.read_csv(os.path.join(DATA_DIR, "stock_data/{}.csv").format(symbol)).to_dict('list'))
+
+        # return render_template('industry.html', data=data)
 
 
 @app.route('/industry/company')
